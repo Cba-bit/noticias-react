aba1ec959168f411105eeab7d71bda35f323586c
Ignacio <ignaciotagle26@gmail.com>
2020-12-04 01:17:08 -0300

    colocando la categoria en el state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 function App() {
+    // definir la categoria y noticias
+    const [categoria, setCategoria] = useState('');
+
     return (
         <Fragment>
             <Header titulo='Buscador de Noticias' />
 
             <div className='container-white'>
-                <Formulario />
+                <Formulario setCategoria={setCategoria} />
             </div>
         </Fragment>
     );
