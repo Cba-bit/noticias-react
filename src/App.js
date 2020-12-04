58aca18464344114106aada89dc2075a4e1e239d
Ignacio <ignaciotagle26@gmail.com>
2020-12-03 21:36:30 -0300

    Comenzando el proyecto en React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <Fragment>
+            <Header titulo='Buscador de Noticias' />
+
+            <div className='container-white'>
+                <h1>Formulario acá</h1>
+            </div>
+        </Fragment>
+    );
+}
+
+export default App;
